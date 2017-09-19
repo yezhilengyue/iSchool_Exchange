50dfc0fb242afa0d3e94c520b200fdf53130b93b
yezhilengyue <asdf4862@qq.com>
2017-09-19 11:15:26 -0500

    Initial Commit

---
+++ iSchool ExchangeTests/iSchool_ExchangeTests.swift
@@ -0,0 +1,36 @@
+//
+//  iSchool_ExchangeTests.swift
+//  iSchool ExchangeTests
+//
+//  Created by ChenYuwei on 2017/9/19.
+//  Copyright © 2017年 ChenYuwei. All rights reserved.
+//
+
+import XCTest
+@testable import iSchool_Exchange
+
+class iSchool_ExchangeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
