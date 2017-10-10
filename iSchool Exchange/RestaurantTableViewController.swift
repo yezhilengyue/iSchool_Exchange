1c8a78d0b943df45955fefbc71b10528b5e0da6b
yezhilengyue <asdf4862@qq.com>
2017-10-10 13:08:20 -0500

    crushing on10.0

--- iSchool Exchange/RestaurantTableViewController.swift
+++ iSchool Exchange/RestaurantTableViewController.swift
@@ -30,6 +30,8 @@ class RestaurantTableViewController: UITableViewController {
                                "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York","London", "London", "London", "London"]
     
     var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var restaurantIsVisited = Array(repeating: false, count: 21)
 
 
     override func viewDidLoad() {
@@ -70,6 +72,8 @@ class RestaurantTableViewController: UITableViewController {
         cell.locationLabel?.text = restaurantLocations[indexPath.row]
         cell.thumbnailImageView?.image = UIImage(named: restaurantImages[indexPath.row]);
         
+        cell.accessoryType = restaurantIsVisited[indexPath.row] ? .checkmark : .none
+        
         return cell
     }
     
@@ -78,21 +82,36 @@ class RestaurantTableViewController: UITableViewController {
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
-        let callActionHandler = {(action: UIAlertAction!) -> Void in
-            let alertMessage = UIAlertController(
-                title: "Service unavailable",
-                message: "Sorry, the call feature is not available currently",
-                preferredStyle: .alert)
-            alertMessage.addAction(UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: nil ))
-            self.present(alertMessage, animated: true,completion: nil)
-            
-        }
-        let callAction = UIAlertAction(title: "Make a call", style: .default, handler: callActionHandler);
+        let callAction = UIAlertAction(title: "Make a call", style: .default, handler: {(action: UIAlertAction!) -> Void in
+            let alertmessage = UIAlertController(title: "Service unavailable", message: "Sorry, the call feature is not available currently.", preferredStyle: .alert)
+            alertmessage.addAction(UIAlertAction(title: "Call "+"123-000-\(indexPath.row)", style: .default, handler: nil))
+            self.present(alertmessage,animated: true,completion: nil)
+        });
+        
+        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {(action: UIAlertAction!) -> Void in
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .checkmark
+            self.restaurantIsVisited[indexPath.row] = true
+            })
+        
+        let undoCheckInAction = UIAlertAction(title: "Undo check in", style: .default, handler: {(action: UIAlertAction!) -> Void in
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .none
+            self.restaurantIsVisited[indexPath.row] = false
+        })
         
         optionMenu.addAction(cancelAction)
         optionMenu.addAction(callAction)
         
+        if (self.restaurantIsVisited[indexPath.row] == false){
+            optionMenu.addAction(checkInAction)}
+        else {
+            optionMenu.addAction(undoCheckInAction)}
+        
+        
         present(optionMenu,animated: true,completion: nil)
+        
+        tableView.deselectRow(at: indexPath, animated: false)
     }
     
     
@@ -105,17 +124,28 @@ class RestaurantTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            restaurantNames.remove(at: indexPath.row)
+            restaurantLocations.remove(at:indexPath.row)
+            restaurantTypes.remove(at: indexPath.row)
+            restaurantIsVisited.remove(at: indexPath.row)
+            restaurantImages.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        
+        print("Total item: \(restaurantNames.count)")
+        for name in restaurantNames{
+            print(name)
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
